1bb8e71cd486198f9a88a602cbd20427591330aa
bhaveshchhimwal <socialnetworkbhavesh@gmail.com>
2025-09-26 11:50:14 +0530

    update

--- src/data/experiences.ts
+++ src/data/experiences.ts
@@ -1,7 +1,17 @@
 import { IExperience } from "./types";
 
 const experiences : Array<IExperience> = [
- 
+  {
+    company: "Avayo Technologies",
+    role: "Software Developer",
+    work: [
+      "Developed full-stack client websites using React.js, Node.js, Express.js, and MongoDB.",
+      "Collaborated with team to gather requirements, design solutions, and implement features.",
+      "Managed end-to-end project lifecycle, including planning, development, testing, and deployment.",
+    ],
+    joining_date: "June 2025",
+    ending_date: "July 2025"
+  },
  
 ]
 
